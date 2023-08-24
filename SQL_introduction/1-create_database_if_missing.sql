07e8ae943607a2d0b6b06a95d1dccc7bc517610b
offordile chinonso <nolly9662@gmail.com>
2023-08-24 22:47:04 +0100

    deleting database

--- SQL_introduction/1-create_database_if_missing.sql
+++ SQL_introduction/1-create_database_if_missing.sql
@@ -1,6 +1,10 @@
 -- creating a database if not exist
 
-CREATE SCHEMA hbtn_0c_0
+
+CREATE SCHEMA hbtn_0c_0 
+
+
+
 
 
 
---
+++ SQL_introduction/2-remove_database.sql
@@ -0,0 +1,3 @@
+--deleting the database
+
+DROP DATABASE IF EXISTS hbtn_0c_0;
