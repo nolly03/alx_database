6a61fc35f0398fd3b27ea1cc72538e8080960093
offordile chinonso <nolly9662@gmail.com>
2023-08-27 14:37:18 +0100

    jjj

--- SQL_introduction/4-first_table.sql
+++ SQL_introduction/4-first_table.sql
@@ -3,7 +3,7 @@ USE hbtn_test_db_4;
 
 
 
-CREATE TABLE first_table(
+CREATE TABLE IF NOT EXISTS 7first_table(
     id INT,
     name VARCHAR(256),
 );
--- SQL_introduction/5-full_table.sql
+++ SQL_introduction/5-full_table.sql
@@ -1,3 +1,3 @@
 -- printing the full description os a table
 USE hbtn_0c_0;
-SELECT * FROM first_table;
+SHOW COLUMNs FROM first_table;
