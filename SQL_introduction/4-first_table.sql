286a744bbdb5f28e17335e0b0761340bdf96e1d7
offordile chinonso <nolly9662@gmail.com>
2023-09-08 13:05:04 +0100

    energy regained

--- SQL_introduction/4-first_table.sql
+++ SQL_introduction/4-first_table.sql
@@ -1,9 +1,3 @@
 -- first table
-USE hbtn_test_db_4;
-DROP TABLE IF EXISTS first_table;
 
-
-CREATE TABLE first_table(
-    id INT,
-    name VARCHAR(256),
-);
+CREATE TABLE IF NOT EXISTS `first_table` (`id` INT, `name` VARCHAR(256));
